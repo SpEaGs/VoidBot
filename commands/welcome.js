da60a3c50ee0ab9653560cb855c08cd3988f4ed2
Zach Speagle <speags557k4@gmail.com>
2020-08-30 11:08:53 -0400

    Updated welcome to support single channel commands

--- commands/welcome.js
+++ commands/welcome.js
@@ -16,12 +16,13 @@ module.exports = {
     execute(params) {
         arg = params.args.join(' ')
         let anno = false;
+        let welcomeChannel = params.bot.client.channels.get(params.bot.welcomeTextChannel.id)
         if (params.bot.announcementsRole != false) anno = true;
         if (params.bot.ruleTextChannel != false) {
-            params.msg.channel.sendMessage(utils.welcome(utils.findMemberFromGuild(arg, params.bot.guild), anno)+`\nPlease read the rules in ${params.bot.guild.channels.get(params.bot.ruleTextChannel.id).toString()}`)
+            welcomeChannel.sendMessage(utils.welcome(utils.findMemberFromGuild(arg, params.bot.guild), anno)+`\nPlease read the rules in ${params.bot.guild.channels.get(params.bot.ruleTextChannel.id).toString()}`)
         }
         else {
-            params.msg.channel.sendMessage(utils.welcome(utils.findMemberFromGuild(arg, params.bot.guild), anno));
+            welcomeChannel.sendMessage(utils.welcome(utils.findMemberFromGuild(arg, params.bot.guild), anno));
         }
 
     }
