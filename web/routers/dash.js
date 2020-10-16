e26928a9e23771d5913676218a2c7615d7e31804
Zach Speagle <speags557k4@gmail.com>
2020-10-16 15:57:04 -0400

    add login requirement for /dash and its children

--- web/routers/dash.js
+++ web/routers/dash.js
@@ -2,7 +2,25 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('main');
+    if(!req.user) {
+        res.redirect('/auth/login');
+    }
+    else {
+        res.render('main', {user: req.user});
+    }
+});
+
+router.get('/admin', (req, res) => {
+    if (!req.user) {
+        res.redirect('/auth/login');
+    }
+    if (req.user.guilds.admin === []) {
+        res.render('noAdmin');
+        setTimeout(res.redirect('/dash'), 10 * 1000);
+    }
+    else {
+        res.render('admin', {user: req.user});
+    }
 })
 
 module.exports = router;
