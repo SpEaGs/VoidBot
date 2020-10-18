61cb6111bbbd466c79d41930c3917c39f02f1cf4
Zach Speagle <speags557k4@gmail.com>
2020-10-18 14:18:13 -0400

    add logging to test

--- web/routers/dash.js
+++ web/routers/dash.js
@@ -24,6 +24,7 @@ router.get('/admin', (req, res) => {
     if (!req.user) {
         res.redirect('/auth/login');
     }
+    log(JSON.stringify(req.user.guilds));
     if (req.user.guilds.admin === []) {
         res.render('noAdmin');
         setTimeout(res.redirect('/dash'), 10 * 1000);
