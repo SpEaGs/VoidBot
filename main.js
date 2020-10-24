536e8c158c7cf074608c2631fb7b852ec3345588
Zach Speagle <speags557k4@gmail.com>
2020-10-24 16:33:13 -0400

    fix webUI not receiving new servers when added

--- main.js
+++ main.js
@@ -319,6 +319,7 @@ status.client.on('guildCreate', guild => {
     let newBot = new Bot.Bot(guild, status);
     status.client.children.set(guild.id, newBot);
     initBot(newBot);
+    status.eSender.socket.emit('add-client', newBot);
 });
 
 //discord.js client event for the bot leaving or being kicked from a server
