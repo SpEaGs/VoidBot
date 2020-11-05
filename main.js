7b114e494198d417cd9958d678d32930f1d2745e
Zach Speagle <speags557k4@gmail.com>
2020-11-04 20:52:03 -0500

    add logging to help debug guildMemberAdd client event

--- main.js
+++ main.js
@@ -339,6 +339,7 @@ status.client.on('guildDelete', guild => {
 //discord.js client event for new members joining a server
 status.client.on('guildMemberAdd', member => {
     let bot = status.client.children.get(member.guild.id);
+    log(`New Member Joined: ${member.user.username} Welcome Message set to: ${bot.welcomeMsg}`, `[${bot.guildName}]`);
     try {
         if (bot.welcomeMsg == false) return;
         if (bot.welcomeTextChannel != false) {
