a9e44771890d38dc15563f1cedb8bf43fe2424e2
Zach Speagle <speags557k4@gmail.com>
2020-11-05 00:14:38 -0500

    please?

--- commands/play.js
+++ commands/play.js
@@ -120,7 +120,8 @@ async function createStream(status, info, msg) {
             str = ytdl.downloadFromInfo(info, { filter: 'audioonly' });
         }
         case 'SC': {
-            sc.download(info.url, SC_API_KEY).then(stream => { str = stream.pipe(fs.createWriteStream('temp.mp3')); });
+            await sc.download(info.url, SC_API_KEY).then(stream => { stream.pipe(fs.createWriteStream('temp.mp3')); });
+            str = './temp.mp3';
         }
     }
     if (process.env.NODE_ENV == 'development') { str.on('error', console.error) }
