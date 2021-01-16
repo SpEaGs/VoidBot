345cb39ba3600d1b74a8d5719d105cc7a8aa951d
Zach Speagle <speags557k4@gmail.com>
2021-01-16 14:06:13 -0500

    fix crash when called from WebUI

--- commands/wrongsong.js
+++ commands/wrongsong.js
@@ -18,15 +18,15 @@ module.exports = {
     execute(params) {
         let item = parseInt(params.args)
         if (!params.args.length) {
-            return params.msg.reply(`You need to tell me which song to remove... Use \`${prefix}playlist\` to see a list then give me the number of the song you want to remove.`);
+            return params.bot.guild.channels.cache.get(params.bot.defaultTextChannel.id).send(`You need to tell me which song to remove... Use \`${prefix}playlist\` to see a list then give me the number of the song you want to remove.`);
         }
-        else if (isNaN(item)) return params.msg.reply(`That's not a number you fool.`);
+        else if (isNaN(item)) return params.bot.guild.channels.cache.get(params.bot.defaultTextChannel.id).send(`That's not a number you fool.`);
         else {
             let i = item-1
             let title = params.bot.audioQueue[i].title;
             params.bot.audioQueue.splice(i, 1);
             MAIN.eSender.socket.emit('sendBotInfo', [utils.dumbifyBot(params.bot)]);
-            return params.msg.reply(`Removed \`${title}\` from the queue.`)
+            return params.bot.guild.channels.cache.get(params.bot.defaultTextChannel.id).send(`Removed \`${title}\` from the queue.`)
         }
     }
 };
