a6f51243f65e382e8b3d89e0b9a5295287f90f6f
Zach Speagle <speags557k4@gmail.com>
2021-05-23 13:48:26 -0400

    fix bad var

--- bot.js
+++ bot.js
@@ -59,7 +59,7 @@ class Bot extends EventEmitter {
 
         //save config (should be done after every edit to the config object)
         utils.dumpJSON('./config.json', utils.config, 2);
-        for (let cmd of this.status.cmds.array()) {
+        for (let cmd of this.status.client.cmds.array()) {
             if (cmd.regJSON != false) {
                 this.status.client.api.applications(this.status.client.user.id).guilds(this.guildID).commands.post({data: cmd.regJSON});
             }
