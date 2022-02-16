4ecac1738ed23fd4ce75c1e80c6f2be78744038c
Zach Speagle <speags557k4@gmail.com>
2022-02-16 15:54:48 -0500

    REEEEEE

--- main.js
+++ main.js
@@ -131,11 +131,12 @@ function launchWebServer() {
   api.get("/", (req, res) => {
     console.log(req.user);
     if (!req.user) {
-      res.redirect(utils.config.webAppDomain + "test");
+      res.send(req.user);
+      //res.redirect(utils.config.webAppDomain + "test");
     } else {
-      res.redirect(
-        `${utils.config.webAppDomain}token?dtoken=${req.user.token}`
-      );
+      //res.redirect(
+      //  `${utils.config.webAppDomain}token?dtoken=${req.user.token}`
+      //);
     }
   });
 
