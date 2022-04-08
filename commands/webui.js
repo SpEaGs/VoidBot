adef9b29cd32c219b5c10668dd6f986076e6f5ba
Zach Speagle <speags557k4@gmail.com>
2022-04-08 13:47:17 -0400

    fixed webUI cmd showing old port

--- commands/webui.js
+++ commands/webui.js
@@ -1,29 +1,35 @@
-
 //WebUI command. Sends a link to the Bot's WebUI dashboard.
 
-const utils = require('../utils.js');
+const utils = require("../utils.js");
 const prefix = utils.config.prefix;
 const pubIP = utils.config.pubIP;
 
-let name = 'webui'
-let description = 'Sends a link to the WebUI page.'
+let name = "webui";
+let description = "Sends a link to the WebUI page.";
 
 module.exports = {
+  name: name,
+  description: description,
+  alias: [],
+  args: false,
+  usage: `\`${prefix}webui\``,
+  admin: false,
+  botadmin: false,
+  server: true,
+  execute(params) {
+    let mem = params.msg.member;
+    try {
+      params.msg.reply(
+        `The web UI can be found here: http://${pubIP}:7777/dash`
+      );
+    } catch {
+      params.bot.guild.channels.cache
+        .get(params.bot.defaultTextChannel.id)
+        .send(`${mem} The web UI can be found here: http://${pubIP}:3000`);
+    }
+  },
+  regJSON: {
     name: name,
     description: description,
-    alias: [],
-    args: false,
-    usage: `\`${prefix}webui\``,
-    admin: false,
-    botadmin: false,
-    server: true,
-    execute(params) {
-        let mem = params.msg.member;
-        try { params.msg.reply(`The web UI can be found here: http://${pubIP}:7777/dash`) }
-        catch { params.bot.guild.channels.cache.get(params.bot.defaultTextChannel.id).send(`${mem} The web UI can be found here: http://${pubIP}:7777/dash`) }
-    },
-    regJSON: {
-        name: name,
-        description: description
-    }
-}
+  },
+};
