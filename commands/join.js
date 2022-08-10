811555bf2e289cefc1760318d74058df8424deb5
Zach Speagle <speags557k4@gmail.com>
2022-08-10 11:43:50 -0400

    removed debug logging

--- commands/join.js
+++ commands/join.js
@@ -32,8 +32,6 @@ module.exports = {
     let channel = params.interaction.options.getString("channel");
     if (!channel) {
       let voiceChannel = params.interaction.member.voice.channel;
-      console.log(voiceChannel);
-      console.log(params.bot.voiceChannel);
       if (!!voiceChannel && voiceChannel === params.bot.voiceChannel)
         return params.bot.guild.channels.cache
           .get(params.bot.defaultTextChannel.id)
