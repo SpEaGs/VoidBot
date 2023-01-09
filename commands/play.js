b9377c9bcf446692d0c5e3abbac118651482967a
Zach Speagle <speags557k4@gmail.com>
2023-01-08 19:28:01 -0500

    enabled spotify track links

--- commands/play.js
+++ commands/play.js
@@ -154,7 +154,6 @@ function search(str, mem, params) {
       if (url.includes("/track/")) {
         plID = url.split("/").reverse()[0].split("?")[0];
         log(plID, ["[WARN]", "[PLAY]"]);
-        return;
         let spotifyReqURL = `https://api.spotify.com/v1/tracks/${plID}`;
         request(spotifyReqURL, (error, response) => {
           if (error || !response.statusCode == 200) {
