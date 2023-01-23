ad41787ae16883b32fe9681a1ca517e66383f0e6
Zach Speagle <speags557k4@gmail.com>
2023-01-22 21:48:35 -0500

    fixed ssl running anyway

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
 const api = express();
-const server = require(ssl ? "https" : "http").createServer(api);
+const server = require(!!ssl ? "https" : "http").createServer(api);
 const io = require("socket.io")(server, {
   cors: {
     origin: `https://speags.com/voidbot`,
@@ -287,7 +287,7 @@ function launchWebServer() {
     socket.emit("handshake");
   });
 
-  server.listen(ssl ? 2083 : 5000, () => {
+  server.listen(!!ssl ? 2083 : 5000, () => {
     const port = server.address().port;
     log(`API started at port: ${port}`, ["[INFO]", "[WEBSERVER]"]);
   });
