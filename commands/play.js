281b728b3051ed71da93258dc27f626aa9e61244
Zach Speagle <speags557k4@gmail.com>
2023-03-15 21:26:42 -0400

    hoh boy i is havin fun

--- commands/play.js
+++ commands/play.js
@@ -335,7 +335,7 @@ function createStream(status, info) {
           status.dispatcher.once(voice.AudioPlayerStatus.Idle, () => {
             log("Voice Idle", ["[WARN]", "[PLAY]"]);
             endDispatcher(status);
-            fs.unlinkSync(str);
+            //fs.unlinkSync(str);
           });
           status.dispatcher.once("error", (err) => {
             log(`Audio stream error:\n${err}`, ["[ERR]", "[PLAY]"]);
