5bb6d096a8695f22d57c72572ca7adbe5a1fe7f8
Zach Speagle <speags557k4@gmail.com>
2023-07-15 20:07:05 -0400

    webhook test x2

--- utils.js
+++ utils.js
@@ -186,9 +186,9 @@ function populateAdmin(bot) {
 //populates an internal list of commands
 function populateCmds(status) {
   let cmdReg = [];
-
   log("Populating commands list...", ["[INFO]", "[UTILS]"]);
   let cmdFiles = fs.readdirSync("./commands/");
+
   status.client.cmds.clear();
   let togglesOBJ = {};
   for (let file of cmdFiles) {
