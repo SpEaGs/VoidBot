aff3ccd3a54816344f742000e218b2466bf3ac72
Zach Speagle <speags557k4@gmail.com>
2023-09-01 08:29:07 -0400

    added some debug logging to websocket connection event

--- main.js
+++ main.js
@@ -181,6 +181,7 @@ function launchWebServer() {
   }
 
   io.on("connection", (socket) => {
+    log("New websocket connection...", ["[WARN]", "[WS]"]);
     socket.once("get_console", (snowflake) => {
       if (utils.config.botAdmin.includes(snowflake)) {
         status.consoleSockets.set(socket.id, socket);
