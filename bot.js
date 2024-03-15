27ba82c921694a2c38dac0d90861894a43961c1f
Zach Speagle <speags557k4@gmail.com>
2024-03-15 13:30:48 -0400

    i are a dumb

--- bot.js
+++ bot.js
@@ -35,6 +35,7 @@ class Bot extends EventEmitter {
     this.voiceChannel = false;
     this.voiceChannelArray = [];
     this.voiceConnection = false;
+    this.textChannelArray = [];
     this.audioQueue = [];
     this.nowPlaying = false;
 
