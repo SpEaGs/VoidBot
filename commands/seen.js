f96d6b3475d12d386a7698abb737d4a97adb9c29
Zach Speagle <speags557k4@gmail.com>
2024-06-18 18:35:46 -0400

    more debug... wooo

--- commands/seen.js
+++ commands/seen.js
@@ -28,7 +28,7 @@ module.exports = {
       await params.interaction.reply({ content: "Command received!" });
     let mem = params.interaction.member;
     let target = params.interaction.options.getUser("user");
-    console.warn(target);
+    console.warn(status.client.lastSeen[target.id]);
     let timeDiff = utils.getTimeRaw() - status.client.lastSeen[target.id];
     let seen = utils.msToTime(timeDiff);
     if (!target.presence) {
