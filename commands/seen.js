7abcb3698c1a276293f9fcaf31dbc1baef3e6025
Zach Speagle <speags557k4@gmail.com>
2024-06-20 08:30:34 -0400

    REEE

--- commands/seen.js
+++ commands/seen.js
@@ -28,7 +28,7 @@ module.exports = {
       await params.interaction.reply({ content: "Command received!" });
     let mem = params.interaction.member;
     let target = params.interaction.options.getUser("user");
-    console.warn(status.client.lastSeen[target.id]);
+    console.warn(target);
     let timeDiff = utils.getTimeRaw() - status.client.lastSeen[target.id];
     let seen = utils.msToTime(timeDiff);
     if (!target.presence) {
