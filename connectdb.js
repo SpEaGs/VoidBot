6bbf50827404374acb4f6dbc3e643a5d5b175c95
Zach Speagle <speags557k4@gmail.com>
2024-06-27 16:32:37 -0400

    did i won?

--- connectdb.js
+++ connectdb.js
@@ -1,11 +1,11 @@
 const db = require("mongoose");
 const url = process.env.MONGO_DB_CONNECTION_STRING;
 
-const connect = async () => {
-  return await db
-    .connect(url)
-    .then((db) => {
+const connect = () => {
+  db.connect(url)
+    .then((database) => {
       log("Connected to DB", ["[INFO]", "[DB]"]);
+      return db.connection;
     })
     .catch((err) => {
       log(`Error connecting to DB:\n ${err}`, ["[ERR]", "[DB]"]);
