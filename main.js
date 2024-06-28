7cb4ceba1383932a7356d7a93bc02c355d05a702
Zach Speagle <speags557k4@gmail.com>
2024-06-28 13:50:43 -0400

    REEEEE

--- main.js
+++ main.js
@@ -270,6 +270,15 @@ try {
       launchWebServer();
     }, 200);
 
+    utils.cleanUpSockets(status);
+    utils.cleanUpAudioCache(status);
+    setInterval(() => {
+      utils.cleanUpSockets(status);
+    }, 1000 * 60 * 5);
+    setInterval(() => {
+      utils.cleanUpAudioCache(status);
+    }, 1000 * 60 * 60);
+
     log("VoidBot Ready! Hello World!", ["[INFO]", "[MAIN]"]);
   });
 } catch (error) {
@@ -483,13 +492,4 @@ process.on("uncaughtException", (err) => {
     }, 3000);
   } catch {}
 });
-
-utils.cleanUpSockets(status);
-utils.cleanUpAudioCache(status);
-setInterval(() => {
-  utils.cleanUpSockets(status);
-}, 1000 * 60 * 5);
-setInterval(() => {
-  utils.cleanUpAudioCache(status);
-}, 1000 * 60 * 60);
 clientLogin(token);
