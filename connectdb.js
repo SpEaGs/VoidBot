3f4e05756c464e5aae6bddee4e932a3e6fb9cd1c
Zach Speagle <speags557k4@gmail.com>
2024-06-28 08:54:42 -0400

    increased db connection timeouts to 1 min

--- connectdb.js
+++ connectdb.js
@@ -3,7 +3,7 @@ const url = process.env.MONGO_DB_CONNECTION_STRING;
 
 const connect = async () => {
   await db
-    .connect(url)
+    .connect(url, { socketTimeoutMS: 1000 * 60, connectTimeoutMS: 1000 * 60 })
     .then((database) => {
       log("Connected to DB", ["[INFO]", "[DB]"]);
     })
