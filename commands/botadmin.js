c9ace16f75b1e68d395bfebf5c315dad040565fe
Zach Speagle <speags557k4@gmail.com>
2024-07-05 11:51:34 -0400

    did i won?

--- commands/botadmin.js
+++ commands/botadmin.js
@@ -50,7 +50,7 @@ module.exports = {
         const chunkedChoices = [];
         let chunk = 0;
         config.cmdToggles.forEach((i) => {
-          if (!chunkedChoices[chunk]) chunkedChoices[chunk] = [];
+          if (!chunkedChoices[chunk]) chunkedChoices[chunk] = new Array();
           if (chunkedChoices[chunk].length == 25) chunk += 1;
           chunkedChoices[chunk].push(
             new StringSelectMenuOptionBuilder()
