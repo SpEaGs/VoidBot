17e1b20c3da6355255adcdac542dc3d8667726c8
Zach Speagle <speags557k4@gmail.com>
2024-07-05 13:19:05 -0400

    WHAAAAYYYYY??

--- commands/botadmin.js
+++ commands/botadmin.js
@@ -74,10 +74,9 @@ module.exports = {
         try {
           const cmdSelected = await res.awaitMessageComponent({ time: 60_000 });
           const cmd = cmdSelected.values[0];
+          warn(config.cmdToggles.find((i) => i.name === cmd));
           warn(`Toggling command: ${cmd}`, ["[BOTADMIN]"]);
-          toggleBool(
-            config.cmdToggles.find((i) => i.name === cmd.toLowerCase()).state
-          );
+          toggleBool(config.cmdToggles.find((i) => i.name === cmd).state);
           params.interaction.editReply({
             content: `Toggling ${cmd}`,
             components: [],
