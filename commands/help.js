ea4650d89e2bdb646e28f7ba8d011825e29df099
Zach Speagle <speags557k4@gmail.com>
2024-07-06 18:34:37 -0400

    updated help command to list all possible commands in chunks (similar to botadmin toggles)

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,15 @@
 //help command. Displays a list of commands. If given a command name as an arg, displays that command's
 //description and usage.
 
-const { SlashCommandBuilder } = require("discord.js");
+const config = require("../cfg.js");
+const {
+  SlashCommandBuilder,
+  StringSelectMenuBuilder,
+  StringSelectMenuOptionBuilder,
+  ActionRowBuilder,
+} = require("discord.js");
+
+const { log, warn, err } = require("../logger.js");
 
 let name = "Help";
 let description =
@@ -10,13 +18,7 @@ let description =
 module.exports = {
   data: new SlashCommandBuilder()
     .setName(name.toLowerCase())
-    .setDescription(description)
-    .addStringOption((option) =>
-      option
-        .setName("command")
-        .setDescription("The command to get help with")
-        .setRequired(false)
-    ),
+    .setDescription(description),
   name: name,
   description: description,
   args: false,
@@ -26,55 +28,59 @@ module.exports = {
   server: false,
   async execute(params) {
     if (!params.WS)
-      await params.interaction.reply({ content: "Command received!" });
-    let mem = params.interaction.member;
-    let command = params.interaction.options.getString("command");
-    if (!command) {
-      let commandArray = [];
-      params.bot.status.client.cmds.forEach((c) => {
-        commandArray.push(`/${c.name}`);
+      await params.interaction.reply({
+        content: "Command received!",
+        ephemeral: true,
       });
-      return params.WS
-        ? params.bot.guild.channels.cache
-            .get(params.bot.defaultTextChannel.id)
-            .send(`${mem} Commands: \`${commandArray.join("`, `")}\``)
-        : params.interaction.editReply({
-            content: `${mem} Commands: \`${commandArray.join("`, `")}\``,
-          });
-    }
-    if (command === "all") {
-      let toReturnArray = [];
-      params.bot.status.client.cmds.forEach((c) => {
-        toReturnArray.push(
-          `\`/${c.name}\`:\n    Usage: ${c.usage}\n    ${c.description}`
-        );
-      });
-      return params.WS
-        ? params.bot.guild.channels.cache
-            .get(params.bot.defaultTextChannel.id)
-            .send(`${mem} \n${toReturnArray.join(`\n\n`)}`)
-        : params.interaction.editReply({
-            content: `${mem} \n${toReturnArray.join(`\n\n`)}`,
-          });
-    }
-    if (!params.bot.status.client.cmds.has(command)) {
-      return params.WS
-        ? params.bot.guild.channels.cache
-            .get(params.bot.defaultTextChannel.id)
-            .send(`${mem} The command you asked for help with doesn\'t exist.`)
-        : params.interaction.editReply({
-            content: `${mem} The command you asked for help with doesn\'t exist.`,
-          });
-    }
-    let cmd = params.bot.status.client.cmds.get(command);
-    return params.WS
-      ? params.bot.guild.channels.cache
-          .get(params.bot.defaultTextChannel.id)
-          .send(
-            `${mem} \n\`/${cmd.name}\`:\nUsage: ${cmd.usage}\n${cmd.description}`
-          )
-      : params.interaction.editReply({
-          content: `${mem} \n\`/${cmd.name}\`:\nUsage: ${cmd.usage}\n${cmd.description}`,
+    const chunkedChoices = [];
+    let chunk = 0;
+    params.bot.status.client.cmds.forEach((c) => {
+      if (!chunkedChoices[chunk]) chunkedChoices[chunk] = [];
+      chunkedChoices[chunk].push(
+        new StringSelectMenuOptionBuilder()
+          .setLabel(`/${c.name.toLowerCase()}`)
+          .setValue(c.name.toLowerCase())
+      );
+      if (chunkedChoices[chunk].length == 25) chunk++;
+    });
+    const cmdRows = chunkedChoices.map((chnk, i) => {
+      return new ActionRowBuilder().addComponents(
+        new StringSelectMenuBuilder()
+          .setCustomId(`cmd${i + 1}`)
+          .setPlaceholder(`Select a command (page ${i + 1})`)
+          .addOptions(...chnk)
+      );
+    });
+    const res = await params.interaction.editReply({
+      content: `All commands are listed in these menus. Select one to get help with.\nYou can select "all" at the bottom of the last page to get help with every command.\n(Note that some commands are only accessible to those with admin permissions)`,
+      components: cmdRows,
+    });
+    try {
+      const selected = await res.awaitMessageComponent({ time: 120_000 });
+      const sel = selected.values[0];
+      if (sel === "all") {
+        const usageArr = params.bot.status.client.cmds
+          .map((c) => {
+            return `\`/${c.name}\`:\n    Usage: ${c.usage}\n    ${c.description}`;
+          })
+          .join(`\n\n`);
+        return await params.interaction.editReply({
+          content: usageArr,
+          components: [],
         });
+      } else {
+        const cmd = params.bot.status.client.cmds.get(sel);
+        return await params.interaction.editReply({
+          content: `\`${cmd.name}\`:\nUsage: ${cmd.usage}\n${cmd.description}`,
+          components: [],
+        });
+      }
+    } catch (e) {
+      warn(e, ["[INTERACTION]"]);
+      return await params.interaction.editReply({
+        content: `Canceled. No option was selected within two minutes.`,
+        components: [],
+      });
+    }
   },
 };
