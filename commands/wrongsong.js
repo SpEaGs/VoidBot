92db2bd76ae65d3cff6c614f4bd84bb0a95e7f7d
Zach Speagle <speags557k4@gmail.com>
2024-07-08 08:49:32 -0400

    updated wrongsong to use dropdowns for easier use. Slash command limited to first 25 songs in queue, but the WebUI can do all.

--- commands/wrongsong.js
+++ commands/wrongsong.js
@@ -1,23 +1,20 @@
 //Wrongsong command. Used to remove a song from the queue.
 
 const utils = require("../utils.js");
-const { SlashCommandBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  StringSelectMenuBuilder,
+  StringSelectMenuOptionBuilder,
+  ActionRowBuilder,
+} = require("discord.js");
 
 let name = "Wrongsong";
-let description = "Removes a song from the queue.";
+let description = "Removes the selected song from the first 25 songs in queue.";
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName(name.toLowerCase())
-    .setDescription(description)
-    .addIntegerOption((option) =>
-      option
-        .setName("number")
-        .setDescription(
-          "The queue position of the song to remove. (find it with /playlist)"
-        )
-        .setRequired(true)
-    ),
+    .setDescription(description),
   name: name,
   description: description,
   args: false,
@@ -26,48 +23,51 @@ module.exports = {
   botadmin: false,
   server: true,
   async execute(params) {
-    if (!params.WS)
-      await params.interaction.reply({ content: "Command received!" });
-    let mem = params.interaction.member;
-    let number;
-    if (params.WS) number = params.interaction.args.number;
-    else number = params.interaction.options.getInteger("number");
-    if (number > params.bot.audioQueue.length) {
-      return params.WS
-        ? params.bot.guild.channels.cache
-            .get(params.bot.defaultTextChannel.id)
-            .send(`${mem} The playlist isn't even ${number} songs long...`)
-        : params.interaction.editReply({
-            content: `${mem} The playlist isn't even ${number} songs long...`,
-          });
-    } else if (number < 0) {
-      return params.WS
-        ? params.bot.guild.channels.cache
-            .get(params.bot.defaultTextChannel.id)
-            .send(`${mem} That was a negative number...`)
-        : params.interaction.editReply({
-            content: `${mem} That was a negative number...`,
-          });
-    } else if (isNaN(number)) {
-      return params.WS
-        ? params.bot.guild.channels.cache
-            .get(params.bot.defaultTextChannel.id)
-            .send(`${mem} That's not a number you fool.`)
-        : params.interaction.editReply({
-            content: `${mem} That's not a number you fool.`,
-          });
-    } else {
-      let i = number - 1;
-      let title = params.bot.audioQueue[i].videoDetails.title;
-      params.bot.audioQueue.splice(i, 1);
+    const mem = params.interaction.member;
+    if (params.WS) {
+      const index = params.interaction.args.number - 1;
+      const title = params.bot.audioQueue[index].videoDetails.title;
+      params.bot.audioQueue.splice(index, 1);
       utils.informClients(params.bot, { audioQueue: params.bot.audioQueue });
-      return params.WS
-        ? params.bot.guild.channels.cache
-            .get(params.bot.defaultTextChannel.id)
-            .send(`${mem} Removed \`${title}\` from the queue.`)
-        : params.interaction.editReply({
-            content: `${mem} Removed \`${title}\` from the queue.`,
-          });
+      return params.bot.guild.channels.cache
+        .get(params.bot.defaultTextChannel.id)
+        .send(`${mem} Removed \`${title}\` from the queue.`);
+    } else {
+      const truncQueue = params.bot.audioQueue.slice(0, 25);
+      const songRow = new ActionRowBuilder().addComponents(
+        new StringSelectMenuBuilder()
+          .setCustomId("song")
+          .setPlaceholder("Select a song from the first 25 in the queue")
+          .addOptions(
+            ...truncQueue.map((qi) => {
+              return new StringSelectMenuOptionBuilder()
+                .setLabel(qi.videoDetails.title)
+                .setValue(qi.videoDetails.title);
+            })
+          )
+      );
+      const res = await params.interaction.editReply({
+        content: "",
+        components: [songRow],
+      });
+      try {
+        const selected = await res.awaitMessageComponent({ time: 60_000 });
+        const title = selected.values[0];
+        const index = params.bot.audioQueue.findIndex(
+          (song) => song.videoDetails.title === title
+        );
+        params.bot.audioQueue.splice(index, 1);
+        utils.informClients(params.bot, { audioQueue: params.bot.audioQueue });
+        return await params.interaction.editReply({
+          content: `Removed \`${title}\` from the queue.`,
+        });
+      } catch (e) {
+        warn(e, ["[INTERACTION]"]);
+        return await params.interaction.editReply({
+          content: "No song selected within one minute. Canceling...",
+          components: [],
+        });
+      }
     }
   },
 };
